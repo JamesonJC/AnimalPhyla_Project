77cb9fa4d065cfb518a85e07a0b10c2d4f733e28
JamesonJC <jamesonpillay@gmail.com>
2024-02-25 07:39:03 +0200

    Port fix

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from app import app
 import os
 
-port = int(os.environ.get("PORT", 5000))
+#port = int(os.environ.get("PORT", 5000))
 app.run(debug=True)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    #port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, port=8000)
